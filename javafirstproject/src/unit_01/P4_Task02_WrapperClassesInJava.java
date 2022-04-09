d9805e48dd3ab078dbb857bed97bbbddc4e5a998
Anusha <anushabrawat@gmail.com>
2022-04-09 19:21:22 +0530

    commit

---
+++ javafirstproject/src/unit_01/P4_Task02_WrapperClassesInJava.java
@@ -0,0 +1,31 @@
+package unit_01;
+
+public class P4_Task02_WrapperClassesInJava {
+
+	public static void main(String[] args) {
+		
+		//Converting int into integer 
+				int a = 20;
+				
+				String s = "" + a + "";
+				
+				Integer i= Integer.valueOf(a); //Converting int into Integer explicitly 
+				Integer j = a; // autoboxing, now  compiler will write Integer.valueof(a)internally
+				
+				System.out.println(a + " " + " " +j);
+				System.out.println(i.toString());
+				//a.toString() can not be done due to a is int not object 
+				
+				//Autoboxing: Converting primitives into objects 
+				byte b = 10;
+				Byte byteobj = b;
+				
+				System.out.println(byteobj);
+				
+				//Unboxing: Converting Objects to Primitives 
+				byte bytevalue = byteobj;
+				System.out.println(bytevalue);
+
+	}
+
+}
